3a10fabe1f34acfecdd5440fe0a6c8a32469aae7
NancyOsalaniya <69629001+NancyOsalaniya@users.noreply.github.com>
2020-09-22 10:18:04 +0530

    Add files via upload

---
+++ Student/connection.php
@@ -0,0 +1,6 @@
+<?php
+
+$con=mysqli_connect('student-db-instance.ca21f24m2kep.us-east-1.rds.amazonaws.com
+','student_user','Nirma2353nirma','student') or die("CONNECTION UNSUCCESSFULL...!");
+
+?>
---
+++ Student/main_header.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="css\main_header.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div>
+	
+	<ul class="main_menu">
+		<li><a href="add_patient.php">ADD PATIENT</a></li>
+		<li><a href="view_patient.php">VIEW PATIENT</a></li>
+		<li><a href="add_room.php">ADD ROOM</a></li>
+		<li><a href="view_room.php">VIEW ROOM</a></li>
+		<li><a href="add_doctor.php">ADD DOCTOR</a></li>
+		<li><a href="view_doctor.php">VIEW DOCTOR</a></li>
+		<li><a href="logout.php">LOGOUT</a></li>
+	</ul>
+
+</div>
+
+
+<div class="container-fluid">
+	<div class="container">
+			<h1 class="text-center">Our Services</h1>
+		<div class="row" style="margin-top:50px;">
+
+			<div class="col-md-4 text-center">
+				<img src="image/clock.png" width="100px" />
+				<h2>24/7 Services</h2>
+				<p>
+					Our hospital provides any services at any time in day or night...Available 24/7 hours...
+					The patient can get emergency services or personal services etc. any services at anytime...
+				</p>
+				<a href="">Read More</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<img src="image/advices.png" width="100px" />
+				<h2>Care Advices</h2>
+				<p>
+					There are many care advisers are available in our hospital...they gives you some trick how you have to take care of yourself...
+					The patient can get care advices...
+				</p>
+				<a href="">Read More</a>
+			</div>
+			<div class="col-md-4 text-center">
+				<img src="image/emergency.png" width="100px" />
+				<h2>Emergency</h2>
+				<p>
+					Our hospital provides emergency services in accidental situations or if the patient is heart patient mean if the patient is suffering from heart-attack...
+					Our hospital also provides many ambulances or 108 for emergency...
+				</p>
+				<a href="">Read More</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h2>Wel-Come...!</h2>
+					Depending on how you obtained the Windows software, this is a license agreement between (i) you and the device manufacturer or software installer that distributes the software with your device;
+				<p>
+				
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h2>About Us</h2>
+					Depending on how you obtained the Windows software, this is a license agreement between (i) you and the device manufacturer or software installer that distributes the software with your device; 
+				<p>
+				
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h2>Our Services</h2>
+					<ul>
+						<li>Health care</li>
+						<li>Medical care</li>
+						<li>Personal Services</li>
+						<li>Medipro</li>
+						<li>Learn Iport</li>
+					</ul>
+				<p>
+					Thus we provide many different services to our patients...
+					Our medical department have all the different medicines for any health care...
+					In personal services the patients can get the home check-up...It take some charges but patient can get services or check-up at home...
+					Medipro services--
+							If patients have any complaints against the medicines or any medical services then this services can fullfill your requirements...
+				</p>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+</body>
+
+
+</html>
